1b37102fcbcec0bb0fd6d83d6747f9bb4b81314e
EpicPuppy613 <61771471+EpicPuppy613@users.noreply.github.com>
2022-05-24 09:24:22 -0700

    cli update

--- cli/sigma-plus/README.md
+++ cli/sigma-plus/README.md
@@ -1,10 +1,6 @@
 # Sigma+
 A map, not a map, but it builds itself through the community
 
-A map, but also a maploader
-
-Branches are built by the community and installed into the map using a cli
-
 ## Installation
 1. Make sure that you have a stable **java** client installed
 2. Make sure that nodejs is installed
--- cli/sigma-plus/index.js
+++ cli/sigma-plus/index.js
@@ -1,4 +1,24 @@
 #! /usr/bin/env node
+
+/*
+
+Sigma+
+Copyright (C) 2022 EpicPuppy613
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
 const readline = require('readline-sync');
 const os = require('os');
 const homedir = os.homedir();
@@ -10,12 +30,13 @@ var mcdir = homedir + "\\AppData\\Roaming\\.minecraft";
 
 if (args[0] == "install") {
     console.log(`${chalk.hex("#FFFFFF")("-- [")}${chalk.hex("#FF3322")("Sigma+")} ${chalk.hex("#22AAFF")("Installer")}${chalk.hex("#FFFFFF")("] --")}`);
-    console.log(chalk.cyan("Sigma+ is a community driven modular Minecraft map."));
-    console.log(chalk.red("Your current Minecraft install directory is " + homedir + "\\AppData\\Roaming\\.minecraft"));
+    console.log('Sigma+  Copyright (C) 2022  EpicPuppy613');
+    console.log(chalk.cyanBright("Sigma+ is a community driven modular Minecraft map."));
+    console.log(chalk.redBright("Your current Minecraft install directory is " + homedir + "\\AppData\\Roaming\\.minecraft"));
     var correct = readline.keyInYNStrict(chalk.greenBright("Is this correct?"));
     while (!correct) {
         mcdir = readline.question(chalk.cyan("Please enter the correct Minecraft directory: "));
-        console.log(chalk.red("Your current Minecraft install directory is " + mcdir));
+        console.log(chalk.redBright("Your current Minecraft install directory is " + mcdir));
         correct = readline.keyInYNStrict(chalk.greenBright("Is this correct?"));
     }
     if (readline.keyInYNStrict(chalk.blueBright("Do you want to install Sigma+?"))) {
@@ -26,7 +47,12 @@ if (args[0] == "install") {
                 fs.rmSync(mcdir + "\\saves\\SigmaPlus", { recursive: true, force: true });
             }
         } catch {overwrite = true;}
-        fs.mkdirSync(mcdir + "\\saves\\SigmaPlus");
+        try {
+            fs.mkdirSync(mcdir + "\\saves\\SigmaPlus");
+        } catch {
+            console.error(chalk.red("Could not create Sigma+ directory, please check your permissions or install Minecraft."));
+            process.exit(1);
+        }
         if (!overwrite) {
             console.warn(chalk.hex("#dddd22")("Sigma+ will not be installed."));
             process.exit(0);
@@ -37,7 +63,11 @@ if (args[0] == "install") {
                 console.log(chalk.red("Error: " + err));
                 process.exit(1);
             }
-            console.log(chalk.greenBright("Sigma+ has been installed."));
+            console.log(chalk.green("Successfully cloned Sigma+."));
+            console.log(chalk.greenBright("Thank you for installing Sigma+!"));
+            console.log(chalk.cyan("It should now appear in your world select menu."));
+            console.log(chalk.cyanBright("You might have to restart Minecraft to see it."));
+            console.log(chalk.yellowBright("You can install branches using ") + chalk.yellowBright.inverse.bold("npx sigma-plus branch install <branch-url>"));
             process.exit(0);
         });
     }
